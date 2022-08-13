aede62ef113c5b1126db54ae678cef17e46cf7f0
admodev <admodevcodes@outlook.com>
2022-08-13 17:58:24 -0300

    init project

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const PORT = process.env.PORT || 8400;
+
+const server = http.createServer(async (req, res) => {
+	// Seteamos la url de la request
+	if (req.url === '/api' && req.method === 'GET') {
+		// Seteamos los headers
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+
+		// Seteamos la response
+		res.write('Hola, Wayni!');
+
+		// Y aca finalizamos la respuesta
+		res.end();
+	} else {
+		// Si no matchea con una ruta existente respondemos con un 404
+		res.writeHead(404, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({ message: 'Ruta no encontrada.' }));
+	}
+});
