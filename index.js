88ab72ff67116a20807e1e352474a1cfc97df67d
admodev <admodevcodes@outlook.com>
2022-08-13 18:38:37 -0300

    finish api

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const http = require('http');
+const User = require('./controller');
+const { getRequestData } = require('./utils');
 const PORT = process.env.PORT || 8400;
 
 const server = http.createServer(async (req, res) => {
 	// Seteamos la url de la request
-	if (req.url === '/api' && req.method === 'GET') {
+	if (req.url === '/api/users' && req.method === 'GET') {
+		const users = await new User().getUsers();
+
 		// Seteamos los headers
 		res.writeHead(200, { 'Content-Type': 'application/json' });
 
-		// Seteamos la response
-		res.write('Hola, Wayni!');
-
-		// Y aca finalizamos la respuesta
-		res.end();
+		// Aca seteamos y finalizamos la respuesta
+		res.end(JSON.stringify(users));
 	} else {
 		// Si no matchea con una ruta existente respondemos con un 404
 		res.writeHead(404, { 'Content-Type': 'application/json' });
 		res.end(JSON.stringify({ message: 'Ruta no encontrada.' }));
 	}
+
+	// TODO: Agregar las demas rutas con sus condicionales correspondientes
 });
 
 server.listen(PORT, () => {
